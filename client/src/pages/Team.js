e405a0a7de67a42256b7fe374d89e4baa92c17f7
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-12-02 10:25:19 -0600

    updated css

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -2,14 +2,15 @@ import React from 'react';
 import "/Users/Juice/Desktop/Homework/WhatTheTap/client/src/Team.css";
 import {Jumbotron,Container} from "react-bootstrap"
 import { CardDeck, Card} from "react-bootstrap";
+
 export default function Home(){
 
     return(
 
 <div>
     
-    <Jumbotron fluid>
-  <Container>
+    <Jumbotron fluid >
+  <Container jumbotron-fluid >
     <h1>Meet Our Team !</h1>
    </Container>
 </Jumbotron>
@@ -22,10 +23,7 @@ export default function Home(){
     <Card.Img variant="top" src="/img/Sam.jpg" />
     <Card.Body>
       <Card.Title>Samantha Covington</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
+     
     </Card.Body>
     <Card.Footer>
     <div class="icon-bar">
@@ -37,10 +35,6 @@ export default function Home(){
     <Card.Img variant="top" src="/img/ben.jpg"/>
     <Card.Body>
       <Card.Title>Benjamin Lee</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
     </Card.Body>
     <Card.Footer>
     <div class="icon-bar">
@@ -52,11 +46,7 @@ export default function Home(){
     <Card.Img variant="top" src="/img/bishop.jpg" />
     <Card.Body>
       <Card.Title>Bishop Johnson</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
+  
     </Card.Body>
     <Card.Footer>
     <div class="icon-bar">
@@ -65,14 +55,10 @@ export default function Home(){
     </Card.Footer>
   </Card>
   <Card>
-    <Card.Img variant="top" src="/img/Jelisa.jpg" />
+    <Card.Img variant="top" src="/img/Jelisa.JPG" />
     <Card.Body>
       <Card.Title>Jelisa Butler</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
+   
     </Card.Body>
     <Card.Footer>
         <div class="icon-bar">
