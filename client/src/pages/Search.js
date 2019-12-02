12b682c67f0d037e405b67cd160268da115f6511
Samantha Covington <samanthanc_08@yahoo.com>
2019-12-02 09:54:01 -0600

    google map finished

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { GoogleApiWrapper } from "google-maps-react";
 
 const styles = {
+  inputDiv: {
+    textAlign: "center",
+    marginBottom: "3%"
+  },
   map: {
-    height: "100%"
+    margin: "0 auto",
+    width: "70%",
+    height: "70vh",
+    marginBottom: "25px"
   }
 };
 
-export default class Search extends Component {
+export class Search extends Component {
   state = {
     zipcode: ""
   };
@@ -24,92 +32,99 @@ export default class Search extends Component {
       "https://maps.googleapis.com/maps/api/geocode/json?address=" +
       zipcode +
       "&key=AIzaSyAIpJqbv9keeACMWP2A_s2irfrQVA5EILs";
-
     this.geocodeZip(url);
   };
 
-  geocodeZip = (url) => {
-    let lat;
-    let lng;
+  geocodeZip = url => {
+    let that = this;
+    let newLat;
+    let newLng;
     var req = new XMLHttpRequest();
     req.responseType = "json";
     req.open("GET", url, true);
     req.onload = function() {
-      var jsonResponse = req.response;
-      lat = jsonResponse.results[0].geometry.location.lat;
-      lng = jsonResponse.results[0].geometry.location.lng;
-      console.log(lat, lng);
+      var jsonResponse = req.response.results[0];
+      console.log("json response = " + jsonResponse);
+      newLat = jsonResponse ? jsonResponse.geometry.location.lat : 33.0787152;
+      newLng = jsonResponse ? jsonResponse.geometry.location.lng : -96.8083063;
+      that.createGoogleMap(newLat, newLng);
     };
     req.send(null);
-    // setTimeout((lat, lng) => this.initMap(lat, lng), 500);
   };
 
+  createGoogleMap = (newLat, newLng) => {
+    let searchLoc = new window.google.maps.LatLng(newLat, newLng);
 
+    let infowindow = new window.google.maps.InfoWindow();
 
-  // initMap = (lat, lng) => {
-  //   var searchLoc = new google.maps.LatLng(lat, lng);
+    let map = new window.google.maps.Map(
+      document.getElementById("google-map"),
+      {
+        center: searchLoc,
+        zoom: 13
+      }
+    );
 
-  //   infowindow = new google.maps.InfoWindow();
+    // Search for bars in searchLoc
+    var request = {
+      location: searchLoc,
+      radius: "10000",
+      type: ["bar"]
+    };
 
-  //   map = new google.maps.Map(document.getElementById("map"), {
-  //     center: searchLoc,
-  //     zoom: 13
-  //   });
+    let service = new window.google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
 
-  //    var request = {
-  //      location: searchLoc,
-  //      radius: "10000",
-  //      type: ["bar"]
-  //   };
+    // new window.google.maps.Map(document.getElementById("google-map"), {
+    //   center: {
+    //     lat: newLat,
+    //     lng: newLng
+    //   },
+    //   zoom: 13
+    // });
 
-  //   service = new google.maps.places.PlacesService(map);
-  //   service.nearbySearch(request, callback);
-  // };
+    function callback(results, status) {
+      if (status == window.google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          createMarker(place);
+        }
+      }
 
-  // callback = (results, status) => {
-  //     if (status == google.maps.places.PlacesServiceStatus.OK) {
-  //       for (var i = 0; i < results.length; i++) {
-  //         var place = results[i];
-  //         var placeid = place.place_id;
-  //         var name = place.name;
-  //         console.log(place);
-  //         createMarker(place);
-  //       }
-  //     }
+      // Creates marker on map
+      function createMarker(place) {
+        var marker = new window.google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
 
-  //     // Creates marker on map
-  //     function createMarker(place) {
-  //       var marker = new google.maps.Marker({
-  //         map: map,
-  //         position: place.geometry.location
-  //       });
+        var placeName = place.name.toUpperCase();
+        var address = place.vicinity;
+        var rating = place.rating;
 
-  //       var placeName = place.name.toUpperCase();
-  //       var address = place.vicinity;
-  //       var rating = place.rating;
+        window.google.maps.event.addListener(marker, "click", function() {
+          infowindow.setContent(
+            '<p id="link-thing"><strong>' +
+              placeName +
+              "</strong></p><p>" +
+              address +
+              "</p><p>Rating: " +
+              rating +
+              '</p><p><a href="http://www.google.com/search?q=' +
+              place.name +
+              '">Google Search</a></p>'
+          );
 
-  //       google.maps.event.addListener(marker, "click", function() {
-  //         infowindow.setContent(
-  //           '<p id="link-thing"><strong>' +
-  //             placeName +
-  //             "</strong></p><p>" +
-  //             address +
-  //             "</p><p>Rating: " +
-  //             rating +
-  //             '</p><p><a href="http://www.google.com/search?q=' +
-  //             place.name +
-  //             '">Google Search</a></p>'
-  //         );
-
-  //         infowindow.open(map, this);
-  //       });
-  //     }
-  //   }
+          infowindow.open(map, this);
+        });
+      }
+    }
+  };
 
   render() {
     return (
       <div>
-        <div>
+        <div style={styles.inputDiv}>
           <h2>Enter your zipcode to find a brewery near you!</h2>
           <input
             placeholder="Enter your zipcode"
@@ -122,8 +137,12 @@ export default class Search extends Component {
             Submit
           </button>
         </div>
-        <div id="map"></div>
+        <div id="google-map" style={styles.map}></div>
       </div>
     );
   }
 }
+// export default Search;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAIpJqbv9keeACMWP2A_s2irfrQVA5EILs"
+})(Search);
